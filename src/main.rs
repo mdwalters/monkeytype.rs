c1f3b331f33c6ce6ab362aa202c89bd7ba55b292
mdwalters <mail@mdwalters.ml>
2023-06-30 01:38:41 -0400

    feat: it works!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use ansi_term::{Colour, Style};
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut val = String::new();
+
+    print!(
+        "{}",
+        Style::new()
+            .fg(Colour::Fixed(240))
+            .paint("never gonna give you up")
+    );
+    print!("\r");
+
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut val).expect("words here");
 }
